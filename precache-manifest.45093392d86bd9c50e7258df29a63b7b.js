0bfc03c5b330d5fcab3459e0e2f187fd6e5fd081
Robi Hidayat <robihidayatrobi@gmail.com>
2019-10-04 15:38:39 +0700

    Updates

---
+++ precache-manifest.45093392d86bd9c50e7258df29a63b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8e7d6fc21077d9c0c063229c6b46ae",
+    "url": "/robocards/index.html"
+  },
+  {
+    "revision": "212ef36beec83966d354",
+    "url": "/robocards/static/css/main.ffaeba32.chunk.css"
+  },
+  {
+    "revision": "46a625359d5740a7cf12",
+    "url": "/robocards/static/js/2.6982b01f.chunk.js"
+  },
+  {
+    "revision": "212ef36beec83966d354",
+    "url": "/robocards/static/js/main.d2709df5.chunk.js"
+  },
+  {
+    "revision": "51ffed26f6020904478d",
+    "url": "/robocards/static/js/runtime-main.744a6268.js"
+  }
+]);
